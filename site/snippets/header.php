243cd315a9055f5c79dc3d395a2338967bfbeb34
Fabian Beiner <mail@fabian-beiner.de>
2012-10-22 12:30:33 +0200

    Updated <title>, lang="", Charset definition, removed PHP short tags, ...

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo (c::get('lang.current') ? c::get('lang.current') : 'en') ?>">
 <head>
-	
-	<title><?= html($page->title()) ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-	<?= css('assets/styles/styles.css') ?>
-	<?= css('assets/styles/php.css') ?>
-	<?= css('assets/styles/html.css') ?>
-	<?= css('assets/styles/js.css') ?>
-	<?= css('assets/styles/css.css') ?>
+	<meta charset="utf-8">
+	<title><?php echo ($page->title() != '') ? html($page->title() . ' — ') : '' ?><?php echo html($site->title()) ?> by <?php echo html($site->author()) ?></title>
+	<?php echo css('assets/styles/styles.css') ?>
+	<?php echo css('assets/styles/php.css') ?>
+	<?php echo css('assets/styles/html.css') ?>
+	<?php echo css('assets/styles/js.css') ?>
+	<?php echo css('assets/styles/css.css') ?>
 </head>
 
 <body>
-	
-	<div id="wrapper">
-		
+	<div id="wrapper" class="cf">
 		<div class="menu">
-			<a id="logo" href="/"></a>
-			<form id="search" action="/search"><input type="search" name="q" placeholder="Search..."></form>
-			<?php snippet('treemenu') ?>			
+			<a id="logo" href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" alt="Logo"></a>
+			<form id="search" action="<?php echo url() ?>search">
+				<div>
+					<input type="search" name="q" placeholder="Search...">
+				</div>
+			</form>
+			<?php snippet('navBuilder') ?>
 		</div>
