193835e63a84fb3e38442ec1d3b9511e7556408d
Fabian Beiner <mail@fabian-beiner.de>
2012-10-22 12:29:50 +0200

    <nav> should only be used on major navigation elements.

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,7 +1,5 @@
-<nav class="breadcrumb">
-	<ul>
-		<?php $x = 0; foreach($site->breadcrumb() AS $crumb) { ?>
-			<li><a<?php echo ($crumb->isActive()) ? ' class="active"' : '' ?> href="<?php echo $crumb->url() ?>"><?php if ($x==0) echo 'Home'; else echo $crumb->title(); ?> &rsaquo;</a></li>
-		<?php $x++; } ?>
-	</ul>
-</nav>
+<ul class="breadcrumb">
+	<?php $x = 0; foreach($site->breadcrumb() AS $crumb) { ?>
+		<li><a<?php echo ($crumb->isActive()) ? ' class="active"' : '' ?> href="<?php echo $crumb->url() ?>"><?php if ($x==0) echo 'Home'; else echo $crumb->title(); ?></a></li>
+	<?php $x++; } ?>
+</ul>
